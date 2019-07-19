365f8bdec1305fcb0d4a1902c6556377d65208b9
musicaldoofus <burns.trainingdesign@gmail.com>
2019-07-18 23:08:39 -0700

    Updates

---
+++ precache-manifest.619b101b66dd8df9efc91456eceafdc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1ecc964b27b5ca1aa27dd4e917f7c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bc44061567ad0c5ddc7f",
+    "url": "/portfolio/static/css/main.b39a0961.chunk.css"
+  },
+  {
+    "revision": "aa4444a698f35f9e251d",
+    "url": "/portfolio/static/js/2.93163b17.chunk.js"
+  },
+  {
+    "revision": "bc44061567ad0c5ddc7f",
+    "url": "/portfolio/static/js/main.2fcf167a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
