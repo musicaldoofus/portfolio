f6dfdc61263c170beb8397c175acafee82c3ac59
musicaldoofus <burns.trainingdesign@gmail.com>
2019-12-05 18:58:57 -0800

    Basic typography, layout, and palette choices

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, /*HashRouter,*/ Switch, Route } from 'react-router-dom';
 //import Routes from './Routes';
-import ErrorBoundary from './components/molecules/ErrorBoundary';
-import { Container, Row, Col } from 'react-grid-system';
+//import ErrorBoundary from './components/molecules/ErrorBoundary';
+import { Container, Row, Col, Hidden } from 'react-grid-system';
+import Spacer from './components/atoms/Spacer';
 import Nav from './components/molecules/Nav';
 import Header, { Body } from './components/atoms/Text';
 import './App.css';
-import './typography.css';
 import './media/fonts/geomanist-fonts.css';
+
+const Root = ({match}) => {
+	return (
+		<Col style={{padding: '2em', height: '100%'}} sm={10}>
+			<Header>Hi there, I'm Michael.</Header>
+			<Spacer/>
+			<Body size="lg"> I build learning solutions that <span className="accent">empower people</span> and <span className="accent">create impact</span>.</Body>
+		</Col>
+	);
+}
+
+const Routes = () => {
+	return (
+		<Switch>
+			<Route
+				exact
+				path="/"
+				component={Root}
+			/>
+		</Switch>
+	);
+}
 
 const App = () => {
 	return (
 		<div className="app">
-			<ErrorBoundary>
-				<Container fluid debug style={{height: '100%'}}>
-					<Row debug style={{height: '100%'}}>
-						<Col debug sm={2}>
-							<Nav/>
-						</Col>
-						<Col debug style={{padding: '1.5em'}} sm={10} style={{height: '100%'}}>
-							<Header>Hi there, I'm Michael.</Header>
-							<Body size="lg"> I build learning solutions that <span className="accent">empower people</span> and <span className="accent">create impact</span>.</Body>
-						</Col>
-					</Row>
-				</Container>
-			</ErrorBoundary>
+			<Container fluid style={{height: '100%'}}>
+				<Row style={{height: '100%'}}>
+					<Nav/>
+					<Router>
+						<Routes/>
+					</Router>
+				</Row>
+			</Container>
 		</div>
 	);
 }
--- src/components/molecules/Nav/index.js
+++ src/components/molecules/Nav/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Col, Visible, Hidden } from 'react-grid-system';
 import './Nav.css';
 
 const Nav = () => {
 	return (
-		<nav className="nav">
-			<div className="nav-brand">MB</div>
-		</nav>
+		<Col sm={2} style={{backgroundColor: 'var(--black-one)'}}>
+			<nav className="nav">
+				<div className="nav-brand">MB</div>
+			</nav>
+			<Hidden xs>
+				<div className="nav-border nav-border-right"></div>
+			</Hidden>
+			<Visible xs>
+				<div className="nav-border nav-border-bottom"></div>
+			</Visible>
+		</Col>
 	);
 }
 
