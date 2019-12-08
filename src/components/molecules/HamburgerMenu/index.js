6fb78ebe1c9e3f57401c661849075098610ef69e
musicaldoofus <burns.trainingdesign@gmail.com>
2019-12-08 13:26:51 -0800

    Added transitions for hamburger menu

--- src/components/molecules/HamburgerMenu/index.js
+++ src/components/molecules/HamburgerMenu/index.js
@@ -1,21 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import './HamburgerMenu.css';
 
 const HamburgerMenu = ({onClick}) => {
+    useEffect(() => setMounted(true),[]);
+
+    const [isMounted, setMounted] = useState(false);
+
+    const handleClose = () => {
+        setMounted(false);
+        window.setTimeout(onClick, 300);
+    }
+
     return (
         <Fragment>
-            <div className="hamburger-menu-panel">
+            <div className={`hamburger-menu-panel${isMounted ? ' mounted' : ''}`}>
                 <Container>
                     <Row>
                         <Col>
                             <div>Panel</div>
-                            <div className="hamburger-menu-panel-close-btn" onClick={onClick}>Close</div>
+                            <div className="hamburger-menu-panel-close-btn" onClick={handleClose}>Close</div>
                         </Col>
                     </Row>
                 </Container>
             </div>
-            <div className="hamburger-menu-overlay">
+            <div className={`hamburger-menu-overlay${isMounted ? ' mounted' : ''}`}>
             </div>
         </Fragment>
     );
