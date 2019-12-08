8983079cac042dfc5bc41330ea475f1c7b5bb142
musicaldoofus <burns.trainingdesign@gmail.com>
2019-12-08 14:29:31 -0800

    Stable nav hamburger animations and layout

--- src/components/molecules/HamburgerMenu/index.js
+++ src/components/molecules/HamburgerMenu/index.js
@@ -12,14 +12,20 @@ const HamburgerMenu = ({onClick}) => {
         window.setTimeout(onClick, 300);
     }
 
+    const fullHeight = {
+        height: '100%'
+    };
     return (
         <Fragment>
             <div className={`hamburger-menu-panel${isMounted ? ' mounted' : ''}`}>
-                <Container>
-                    <Row>
+                <Container style={fullHeight}>
+                    <Row style={fullHeight}>
                         <Col>
-                            <div>Panel</div>
-                            <div className="hamburger-menu-panel-close-btn" onClick={handleClose}>Close</div>
+                            <div className="hamburger-menu-panel-body">Panel</div>
+                            <button className={`hamburger-menu-panel-close-btn${isMounted ? ' mounted' : ''}`} onClick={handleClose}>
+                                <div></div>
+                                <div></div>
+                            </button>
                         </Col>
                     </Row>
                 </Container>
