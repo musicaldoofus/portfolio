22f812b02fd83432414b0e1a63c40bdc73421833
musicaldoofus <burns.trainingdesign@gmail.com>
2019-12-08 11:08:33 -0800

    Basic hamburger menu with transitions dependent on screen size

--- src/components/molecules/Nav/index.js
+++ src/components/molecules/Nav/index.js
@@ -1,5 +1,6 @@
-import React, { Fragment } from 'react';
-import { Col, Visible, Hidden } from 'react-grid-system';
+import React, { Fragment, useState, useEffect } from 'react';
+import { Row, Col, Visible, Hidden } from 'react-grid-system';
+import navBrand from '../../../media/svg/nav-brand.svg';
 import './Nav.css';
 
 const NavBorder = () => {
@@ -21,11 +22,71 @@ const NavBorder = () => {
 	);
 }
 
+const NavBrand = () => {
+	return (
+		<Row style={{width: '100%'}}>
+			<Col>
+				<div className="nav-brand">
+					<img src={navBrand} alt="Michael Burns, Learning Experience Designer"/>
+				</div>
+			</Col>
+		</Row>
+	);
+}
+
+const NavMenuExpanded = () => {
+	return (
+		<div className="nav-menu-expanded"></div>
+	);
+}
+
+const NavHamburgerMenu = () => {
+	useEffect(() => setMounted(true), []);
+
+	const [showMenu, setShowMenu] = useState(false);
+	const [isMounted, setMounted] = useState(false);
+
+	const handleShowToggle = () => setShowMenu(!showMenu);
+
+	return (
+		<div className="nav-menu-hamburger">
+			<div className={`nav-menu-hamburger-lines${isMounted ? ' mounted' : ''}`} onClick={handleShowToggle}>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+			{showMenu && (
+				<Fragment>
+					<div className="nav-menu-hamburger-panel">
+						<div className="nav-menu-hamburger-panel-close">
+						</div>
+					</div>
+					<div className="nav-menu-hamburger-panel-bg"></div>
+				</Fragment>
+			)}
+		</div>
+	);
+}
+
+const NavMenu = () => {
+	return (
+		<Fragment>
+			<Hidden xs>
+				<NavMenuExpanded/>
+			</Hidden>
+			<Visible xs>
+				<NavHamburgerMenu/>
+			</Visible>
+		</Fragment>
+	);
+}
+
 const Nav = () => {
 	return (
 		<Col sm={2}>
 			<nav className="nav">
-				<div className="nav-brand">MB</div>
+				<NavBrand/>
+				<NavMenu/>
 			</nav>
 			<NavBorder/>
 		</Col>
