c336ea2259b7bddf8b4d161099c2b249d5966c7d
musicaldoofus <burns.trainingdesign@gmail.com>
2020-03-11 17:02:24 -0700

    Updates

---
+++ precache-manifest.e9ff6e02d36121b80fef71d127df2f5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1777a62fc16895a36a2abfa4e369d870",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1e5cbe415d1181c9575b",
+    "url": "/portfolio/static/css/main.962d3fcb.chunk.css"
+  },
+  {
+    "revision": "dd5bf5226cdda5b419e1",
+    "url": "/portfolio/static/js/2.da7fd995.chunk.js"
+  },
+  {
+    "revision": "1e5cbe415d1181c9575b",
+    "url": "/portfolio/static/js/main.bde348f1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dd1f15a99ac95826933cfcde885d70b5.js"
+  "/portfolio/precache-manifest.e9ff6e02d36121b80fef71d127df2f5d.js"
 );
 
 self.addEventListener('message', (event) => {
